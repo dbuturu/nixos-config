acf1b6c75f7cbb970a6fb37f284e8a7b7398ba2c
Daniel Kigen Buturu <dbuturu@users.noreply.github.com>
2026-05-26 12:08:25 +0300

    chore: update flake.lock

--- flake.nix
+++ flake.nix
@@ -13,12 +13,12 @@
 
   inputs = {
     # Stable release branch for reliable system packages
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.05"; 
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.11"; 
     # Unstable branch for latest packages (Firefox, development tools, etc.)
     nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
 
     home-manager = {
-      url = "github:nix-community/home-manager/release-25.05";
+      url = "github:nix-community/home-manager/release-25.11";
       inputs.nixpkgs.follows = "nixpkgs"; # Use same nixpkgs version for consistency
     };
 
